8c3cf976eaeaf898577c17dca69d9f15f15e754f
soni <soni@iosandweb.net>
2024-09-16 16:39:50 +0530

    update url

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
        <p className='font-semibold text-xl mb-2'>Important</p> The Ananta Aspire is only acting as a medium for providing online advertising services. The Ananta Aspire does not in any way facilitate and cannot be deemed to be facilitating sales between developers and the visitors/users of the website. The display of information on The Ananta Aspire with respect to a developer or project does not guarantee that the developer / project has registered under the Real Estate (Regulation and Development), 2016 or is compliant with the same. Before deciding to purchase or taking any other action, you are requested to exercise due caution and to independently validate and verify all information about the project.
       </CookieConsent>
 
-      <BrowserRouter >
+      <BrowserRouter basename='/anantaaspire'>
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route index element={<Home />} />
